f97a95f18f95963638bd3f9cda8ba4a409b9d5bc
Extra154 <lewismanzini66@gmail.com>
2025-12-16 09:26:35 -1100

    Update auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const db = require("../utils/db");
+const db = require("../utils/database");
 
 // SIGN UP
 router.post("/signup", (req, res) => {
@@ -55,3 +55,4 @@ router.post("/login", (req, res) => {
 
 
 module.exports = router;
+
