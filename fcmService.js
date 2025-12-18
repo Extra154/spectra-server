80d66e7093d041c36b3d43180df89ccebe7ae936
Extra154 <lewismanzini66@gmail.com>
2025-12-18 12:04:49 +0200

    Update fcmService.js

--- fcmService.js
+++ fcmService.js
@@ -1,16 +1,18 @@
 const admin = require("firebase-admin");
 
-async function sendIncomingCall(username, caller, channel) {
+// This function sends a push notification to a single receiver
+async function sendIncomingCall(username, callerUsername, channelName) {
   const message = {
-    topic: calls_${username},
+    topic: calls_${username}, // topic named after receiver
     data: {
-      type: "incoming_call",
-      caller,
-      channel
+      type: "incoming_call",   // marks this as a call notification
+      callType: "voice",       // specifies it's a voice call
+      caller: callerUsername,  // who is calling
+      channel: channelName     // Agora channel or any channel identifier
     }
   };
 
-  return admin.messaging().send(message);
+  await admin.messaging().send(message);
 }
 
 module.exports = { sendIncomingCall };
